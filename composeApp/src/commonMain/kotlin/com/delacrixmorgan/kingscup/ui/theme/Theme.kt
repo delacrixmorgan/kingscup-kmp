7bb7e330430fb7e666c19b2c91da45066c603bb3
Morgan Koh <delacrixmorgan@pm.me>
2025-03-20 21:41:46 +0100

    Add MadderColorPreference

--- composeApp/src/commonMain/kotlin/com/delacrixmorgan/kingscup/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/delacrixmorgan/kingscup/ui/theme/Theme.kt
@@ -3,12 +3,15 @@ package com.delacrixmorgan.kingscup.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.delacrixmorgan.kingscup.ui.theme.color.BoraThemPreference
 import com.delacrixmorgan.kingscup.ui.theme.color.ColorPreference
+import com.delacrixmorgan.kingscup.ui.theme.color.EmeraldColorPreference
+import com.delacrixmorgan.kingscup.ui.theme.color.MadderColorPreference
 import com.delacrixmorgan.kingscup.ui.theme.color.SandColorPreference
 
 @Composable
 fun AppTheme(
-    colorPreference: ColorPreference = SandColorPreference,
+    colorPreference: ColorPreference = listOf(SandColorPreference, BoraThemPreference, EmeraldColorPreference, MadderColorPreference).random(),
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (!isSystemInDarkTheme()) {
