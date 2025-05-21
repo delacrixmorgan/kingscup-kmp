c30e425daf2cbcc9cedb9c216a657192dedba1b3
Morgan Koh <delacrixmorgan@pm.me>
2025-05-21 23:25:37 +0200

    Animate Board to have LazyRow that elevates and scales

--- composeApp/src/commonMain/kotlin/com/delacrixmorgan/kingscup/ui/board/Board.kt
+++ composeApp/src/commonMain/kotlin/com/delacrixmorgan/kingscup/ui/board/Board.kt
@@ -1,8 +1,13 @@
 package com.delacrixmorgan.kingscup.ui.board
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
@@ -11,14 +16,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.hapticfeedback.HapticFeedback
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -36,8 +57,73 @@ fun BoardRoot(viewModel: BoardViewModel, navHostController: NavHostController) {
 fun BoardScreen(
     state: BoardUiState,
     onAction: (BoardAction) -> Unit,
+    haptic: HapticFeedback = LocalHapticFeedback.current
 ) {
-    onAction(BoardAction.OnCardClicked(""))
+    val cards = (1..52).map { it.toString() }
+    val lazyListState = rememberLazyListState()
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LazyRow(
+            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 48.dp),
+            state = lazyListState,
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            flingBehavior = ScrollableDefaults.flingBehavior(),
+        ) {
+            items(cards) { card ->
+                var isPressed by remember { mutableStateOf(false) }
+                val animatedElevation by animateDpAsState(
+                    targetValue = if (isPressed) 16.dp else 6.dp,
+                    label = "CardElevation"
+                )
+
+                val animatedScale by animateFloatAsState(
+                    targetValue = if (isPressed) 1.05f else 1f,
+                    label = "CardScale"
+                )
+                
+                Card(
+                    modifier = Modifier.size(width = 150.dp, height = 210.dp)
+                        .graphicsLayer {
+                            scaleX = animatedScale
+                            scaleY = animatedScale
+                            shadowElevation = animatedElevation.toPx()
+                            shape = RoundedCornerShape(12.dp)
+                            clip = false
+                        }
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onPress = {
+                                    isPressed = true
+                                    try {
+                                        this.awaitRelease()
+                                    } finally {
+                                        isPressed = false
+                                    }
+                                },
+                                onTap = {
+                                    haptic.performHapticFeedback(HapticFeedbackType.ContextClick)
+                                }
+                            )
+                        },
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = animatedElevation),
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.onPrimaryContainer)
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(card, color = MaterialTheme.colorScheme.onPrimary)
+                    }
+                }
+            }
+        }
+    }
+
     return
     Scaffold(
         content = {
